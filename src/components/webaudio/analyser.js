265a8b88fe8d7cfde1cd38d947d16122ea9fd2fd
George Bennett <george6bennett@gmail.com>
2019-07-14 19:26:38 +0100

    add: frequency canvas to Analyser component

--- src/components/webaudio/analyser.js
+++ src/components/webaudio/analyser.js
@@ -7,47 +7,31 @@ export const AnalyserNodeComponent = props => {
   const [currentMaxVoltage, setCurrentMaxVoltage] = useState(0);
 
   const waveformCanvas = useRef();
+  const frequencyCanvas = useRef();
+  const CANVAS_WIDTH = 150;
+  const CANVAS_HEIGHT = 40;
 
-  const loop = () => {
-    // Ensure node is initialised and is active
-    if (props.analyserNode && !props.analyserNode.bypass) {
-      const bufferLength = props.analyserNode.instance.frequencyBinCount;
-      const dataArray = new Uint8Array(bufferLength);
-      props.analyserNode.instance.getByteTimeDomainData(dataArray);
-      // Update graphs
-      updateLevel(dataArray);
-      updateWaveform(dataArray);
-      // Loop
-      requestAnimationFrame(loop);
-      setLooping(true);
-    } else {
-      clearLevel();
-      clearWaveform();
-      setLooping(false);
-    }
-  };
-
-  const updateLevel = dataArray => {
+  const updateLevelMeter = timeDomainDataArray => {
     // Find max sample
-    const max = Math.max(...dataArray);
+    const max = Math.max(...timeDomainDataArray);
     // Convert 8bit waveform data [0, 255] to voltage [-1,1]
     const maxVoltage = (max - 128) / 128;
     // Set state to trigger re-render
     setCurrentMaxVoltage(maxVoltage);
   };
 
-  const clearLevel = () => {
+  const clearLevelMeter = () => {
     setCurrentMaxVoltage(0);
   };
 
-  const updateWaveform = dataArray => {
+  const updateWaveformCanvas = timeDomainDataArray => {
     if (waveformCanvas) {
       const context = waveformCanvas.current.getContext("2d");
-      context.clearRect(0, 0, 130, 40);
+      context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
       context.beginPath();
-      dataArray.forEach((value, index) => {
-        const y = (value / 128) * 40;
-        const x = index;
+      timeDomainDataArray.forEach((value, index) => {
+        const y = (value / 128) * CANVAS_HEIGHT;
+        const x = index * (CANVAS_WIDTH / timeDomainDataArray.length);
         if (index === 0) {
           context.moveTo(x, y);
         } else {
@@ -58,9 +42,55 @@ export const AnalyserNodeComponent = props => {
     }
   };
 
-  const clearWaveform = () => {
+  const clearWaveformCanvas = () => {
     const context = waveformCanvas.current.getContext("2d");
-    context.clearRect(0, 0, 130, 40);
+    context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  };
+
+  const updateFrequencyCanvas = frequencyDataArray => {
+    if (frequencyCanvas) {
+      const context = frequencyCanvas.current.getContext("2d");
+      context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+      context.beginPath();
+      frequencyDataArray.forEach((value, index) => {
+        // Plot every 2nd bar
+        if (index % 2 === 0) {
+          const y = (value / 255) * CANVAS_HEIGHT;
+          const x = index;
+          context.fillRect(x, CANVAS_HEIGHT - y, 1, CANVAS_HEIGHT);
+        }
+      });
+      context.stroke();
+    }
+  };
+
+  const clearFrequencyCanvas = () => {
+    const context = frequencyCanvas.current.getContext("2d");
+    context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  };
+
+  const loop = () => {
+    // Ensure node is initialised and is active
+    if (props.analyserNode && !props.analyserNode.bypass) {
+      const bufferLength = props.analyserNode.instance.frequencyBinCount;
+      const timeDomainDataArray = new Uint8Array(bufferLength);
+      const frequencyDataArray = new Uint8Array(bufferLength);
+      props.analyserNode.instance.getByteTimeDomainData(timeDomainDataArray);
+      props.analyserNode.instance.getByteFrequencyData(frequencyDataArray);
+
+      // Update graphs
+      updateLevelMeter(timeDomainDataArray);
+      updateWaveformCanvas(timeDomainDataArray);
+      updateFrequencyCanvas(frequencyDataArray);
+      // Loop
+      requestAnimationFrame(loop);
+      setLooping(true);
+    } else {
+      clearLevelMeter();
+      clearWaveformCanvas();
+      clearFrequencyCanvas();
+      setLooping(false);
+    }
   };
 
   useEffect(() => {
@@ -86,12 +116,19 @@ export const AnalyserNodeComponent = props => {
         max="1"
         value={currentMaxVoltage}
       />
+      <label htmlFor="frequency">Frequency:</label>
+      <canvas
+        name="frequency"
+        ref={frequencyCanvas}
+        width={CANVAS_WIDTH + "px"}
+        height={CANVAS_HEIGHT + "px"}
+      />
       <label htmlFor="waveform">Waveform:</label>
       <canvas
         name="waveform"
         ref={waveformCanvas}
-        width="130px"
-        height="40px"
+        width={CANVAS_WIDTH + "px"}
+        height={CANVAS_HEIGHT + "px"}
       />
     </AudioNodeElement>
   );
